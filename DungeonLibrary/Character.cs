b535a47ab24f13ccd9bc92d9a8cdde6ee0f01ba9
Andrew Cocroft <drew.cocroft@outlook.com>
2023-04-25 21:16:06 -0500

    Successfully made a test harness and the required libraries for a running dungeon character creator! NOTE: Ignore the initial project submitted in the morning of the 25th of April, this is the proper project.

---
+++ DungeonLibrary/Character.cs
@@ -0,0 +1,82 @@
+﻿namespace DungeonLibrary
+{
+    public class Character
+    {
+        public static void Header(string title)
+        {
+            Console.Title = title.ToUpper();
+            Console.WriteLine("CSF2: " + title.ToUpper());
+        }//end header
+        /*
+           Create Fields and Properties for each of the following attributes.
+           life – int
+           name – string
+           hitChance – int
+           block – int
+           maxLife – int
+
+            Include a business rule that Life cannot be more than MaxLife. If it is, set it equal to
+            MaxLife
+         */
+        //funny
+        //FIELDS
+
+        //people
+        //PROPERTIES
+
+        //collect
+        //CONSTRUCTORS - Life = life; -> Life = maxLife.
+        //No matter what, assign MaxLife BEFORE Life.
+
+        //monkeys 
+        //METHODS
+
+        private int _life;
+        private int _maxLife;
+        private int _hitChance;
+        private int _block;
+        private string _name;
+
+        public int Life
+        {
+            get { return _life; }
+            set
+            {
+                if (value > _maxLife)
+                    _life = _maxLife;
+                else
+                    _life = value;
+            }
+        }
+        public int MaxLife 
+        {   get { return _maxLife; }
+            set { _maxLife = value; }
+        }
+        public string Name 
+        {   get { return _name; } 
+            set { _name = value; }
+        }
+        public int HitChance
+        {
+            get { return _hitChance; }
+            set { _hitChance = value; }
+        }
+        public int Block
+        {
+            get { return _block; }
+            set { _hitChance = value; }
+        }
+        public Character(int life, string name, int hitChance, int block, int maxLife)
+        {
+            MaxLife = maxLife;
+            Life = life;
+            Name = name;
+            HitChance = hitChance;
+            Block = block;
+        }
+
+        public Character()
+        {
+        }
+    }//end class
+}//end space
---
+++ DungeonLibrary/Weapon.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    //Make it public
+    public class Weapon
+    {
+        /* 
+         Make fields and props for each of these with the appropriate naming conventions.
+         Business rule on MinDamage, can't be more than MaxDamage, or less than 1. If it is, default it to 1.
+            
+        
+            minDamage – int
+            maxDamage – int
+            name – string
+            bonusHitChance – int
+            isTwoHanded - bool
+         
+         */
+
+        //funny
+        //FIELDS
+        //people
+        //PROPERTIES
+        //collect
+        //CONSTRUCTORS
+        //monkeys 
+        //METHODS
+
+        private int _minDamage;
+        private int _maxDamage;
+        private string _name;
+        private int _bonusHitChance;
+        private bool _isTwoHanded; 
+
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set { _minDamage = value; }
+        }public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set { _maxDamage = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set { _bonusHitChance = value; }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
+
+        public Weapon(int minDamage, int maxDamage, string name, int bonusHitChance, bool isTwoHanded)
+        {
+            MaxDamage = maxDamage;
+            MinDamage = minDamage <= MaxDamage ? minDamage : MaxDamage;
+            Name = name;
+            BonusHitChance = bonusHitChance;
+            IsTwoHanded = isTwoHanded;
+        }
+
+        public Weapon()
+        {
+        }
+    }
+}
